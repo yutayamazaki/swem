276e41a18e0ff59e7b60d6fd841f4fa62d657d5e
yutayamazaki <tppymd@gmail.com>
2020-03-15 04:26:47 +0900

    Fix lint

--- tests/test_models.py
+++ tests/test_models.py
@@ -11,12 +11,14 @@ def test_tokenize_ja():
     tokens = models.tokenize_ja('私はバナナです。')
     assert tokens == ['私', 'は', 'バナナ', 'です', '。']
 
+
 def test_tokenize_en():
     """ test for swem.models.tokenize_en """
     text = 'This, is an implementation of SWEM.'
     tokens = models.tokenize_en(text)
     expected = ['This', ',', 'is', 'an', 'implementation', 'of', 'SWEM', '.']
     assert tokens == expected
+
 
 def test_word_embed():
     token = '私'
@@ -84,4 +86,4 @@ class SWEMTests(unittest.TestCase):
         doc_embed = models._doc_embed(doc, self.swem.model.wv, (-1, 1))
         with pytest.raises(ValueError):
             # text_length: 1, n_windows: 3
-            ret = self.swem._hierarchical_pool(doc_embed, n_windows=3)
+            self.swem._hierarchical_pool(doc_embed, n_windows=3)
