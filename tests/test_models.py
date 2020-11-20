ca1635f255d68ce47cb43abefbc855ca5fa57e6e
yutayamazaki <tppymd@gmail.com>
2020-11-20 21:42:44 +0900

    Fix tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,18 +1,12 @@
 import unittest
 from typing import Dict, List, Tuple
 
-from gensim.models.keyedvectors import Word2VecKeyedVectors
-import MeCab
 import numpy as np
 import pytest
+from gensim.models.keyedvectors import KeyedVectors, Word2VecKeyedVectors
 
 import swem
 from swem import models
-
-
-def tokenize_ja(text: str, args: str = '-O wakati') -> List[str]:
-    tagger = MeCab.Tagger(args)
-    return tagger.parse(text).strip().split(' ')
 
 
 def test_load_w2v_success():
@@ -26,23 +20,24 @@ def test_load_w2v_invalid_lang():
 
 
 def test_word_embed():
-    token = '私'
-    kv = MockKV()
+    token: str = '私'
+    kv: KeyedVectors = KeyedVectors(vector_size=200)
     embed = models._word_embed(token, kv=kv)
     assert embed.shape == (200, )
 
 
 def test_word_embeds():
-    tokens = ['私', 'は']
-    kv = MockKV()
+    tokens: List[str] = ['私', 'は']
+    kv: KeyedVectors = KeyedVectors(vector_size=200)
     embed = models._word_embeds(tokens, kv=kv, uniform_range=(-0.01, 0.01))
     assert embed.shape == (2, 200)
 
 
 def test_hierarchical_pool():
-    text: str = 'すもももももももものうち'
-    kv: MockKV = MockKV()
-    word_embeds: np.ndarray = models._word_embeds(text, kv, (-1, 1))
+    tokens: List[str] = ['すもも', 'も', 'もも', 'も', 'もも', 'の', 'うち']
+    kv: KeyedVectors = KeyedVectors(vector_size=200)
+
+    word_embeds: np.ndarray = models._word_embeds(tokens, kv, (-1, 1))
     ret: np.ndarray = models._hierarchical_pool(word_embeds, num_windows=3)
     assert ret.shape == (200, )
 
@@ -50,7 +45,7 @@ def test_hierarchical_pool():
 def test_hierarchical_pool_raise():
     """ ValueError: when invalid n_windows passed. """
     doc: str = '桃'
-    kv: MockKV = MockKV()
+    kv: KeyedVectors = KeyedVectors(vector_size=200)
     word_embeds = models._word_embeds(doc, kv, (-1, 1))
     with pytest.raises(ValueError):
         # text_length: 1, n_windows: 3
@@ -58,8 +53,8 @@ def test_hierarchical_pool_raise():
 
 
 def test_infer_vector_functional():
-    tokens = ['私', 'は', '私', 'は']
-    kv = MockKV()
+    tokens: List[str] = ['私', 'は', '私', 'は']
+    kv: KeyedVectors = KeyedVectors(vector_size=200)
     methods: Dict[str, Tuple[int]] = {
         'avg': (200, ),
         'max': (200, ),
@@ -71,22 +66,11 @@ def test_infer_vector_functional():
         assert embed.shape == shape
 
 
-class MockKV:
-
-    vector_size = 200
-
-    def __getitem__(self, item):
-        return np.zeros(self.vector_size)
-
-
 class SWEMTests(unittest.TestCase):
 
     def setUp(self):
-        self.swem = models.SWEM(MockKV(), tokenize_ja)
-
-    def init_tokenizer_is_not_callable(self):
-        with pytest.raises(ValueError):
-            models.SWEM(MockKV(), 'aaaa')
+        kv: KeyedVectors = KeyedVectors(vector_size=200)
+        self.swem = models.SWEM(kv)
 
     def test_infer_vector(self):
         methods = {
@@ -95,13 +79,13 @@ class SWEMTests(unittest.TestCase):
             'hierarchical': 200,
             'max': 200,
         }
-        doc = 'すもももももももものうち'
+        tokens: List[str] = ['すもも', 'も', 'もも', 'も', 'もも', 'の', 'うち']
         for method_name, embed_dim in methods.items():
-            ret = self.swem.infer_vector(doc, method=method_name)
+            ret = self.swem.infer_vector(tokens, method=method_name)
             assert ret.shape == (embed_dim, )
 
     def test_infer_vector_raise(self):
-        doc = 'すもももももももものうち'
+        tokens: List[str] = ['すもも', 'も', 'もも', 'も', 'もも', 'の', 'うち']
         method = 'invalid method'
         with pytest.raises(ValueError):
-            self.swem.infer_vector(doc, method=method)
+            self.swem.infer_vector(tokens, method=method)
